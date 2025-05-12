f5877193f5a21f5fb791293031c126ba6c4fe738
Portatil <Portatil@DESKTOP-J4PO3AB>
2025-05-12 12:59:37 +0200

    HOLA

--- Reto3AlejandroVictor/src/DAO/PedidoProductoDAO.java
+++ Reto3AlejandroVictor/src/DAO/PedidoProductoDAO.java
@@ -1,5 +1,13 @@
 package DAO;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import Clases.PedidoProducto;
+
 public class PedidoProductoDAO {
+	public static List<PedidoProducto> lista(){
+		List <PedidoProducto> lista=new ArrayList<PedidoProducto>();
+	}
 
 }
