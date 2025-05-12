f13932a327d5d6479c2476faa29bf2fdd61c149f
Vixx-gs <victorgallegoserrate@gmail.com>
2025-05-12 12:31:32 +0200

    Merge branch 'main' of https://github.com/Vixx-gs/Reto3AlejandroVictor.git into main

---
+++ Reto3AlejandroVictor/src/Clases/Pedido.java
@@ -0,0 +1,57 @@
+package Clases;
+
+import java.time.LocalDate;
+
+public class Pedido {
+	private int idpedido;
+	private Cliente idcliente;
+	private double precioTotal;
+	private String direccionEnvio;
+	private LocalDate fecha;
+	public Pedido(int idpedido, Cliente idcliente, double precioTotal, String direccionEnvio, LocalDate fecha) {
+		super();
+		this.idpedido = idpedido;
+		this.idcliente = idcliente;
+		this.precioTotal = precioTotal;
+		this.direccionEnvio = direccionEnvio;
+		this.fecha = fecha;
+	}
+	public Pedido() {
+		super();
+	}
+	public int getIdpedido() {
+		return idpedido;
+	}
+	public void setIdpedido(int idpedido) {
+		this.idpedido = idpedido;
+	}
+	public Cliente getIdcliente() {
+		return idcliente;
+	}
+	public void setIdcliente(Cliente idcliente) {
+		this.idcliente = idcliente;
+	}
+	public double getPrecioTotal() {
+		return precioTotal;
+	}
+	public void setPrecioTotal(double precioTotal) {
+		this.precioTotal = precioTotal;
+	}
+	public String getDireccionEnvio() {
+		return direccionEnvio;
+	}
+	public void setDireccionEnvio(String direccionEnvio) {
+		this.direccionEnvio = direccionEnvio;
+	}
+	public LocalDate getFecha() {
+		return fecha;
+	}
+	public void setFecha(LocalDate fecha) {
+		this.fecha = fecha;
+	}
+	@Override
+	public String toString() {
+		return "Pedido [idpedido=" + idpedido + ", precioTotal=" + precioTotal + ", direccionEnvio=" + direccionEnvio
+				+ ", fecha=" + fecha + "]";
+	}
+}
