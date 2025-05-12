74b3e498e9993118632546996b9581e7a4e49cc7
Portatil <Portatil@DESKTOP-J4PO3AB>
2025-05-12 12:31:59 +0200

    Clase pedidoproducto y cliente

---
+++ Reto3AlejandroVictor/src/Clases/PedidoProducto.java
@@ -0,0 +1,71 @@
+package Clases;
+
+public class PedidoProducto {
+	private int idPedidoProducto;
+	private Producto idProducto;
+	private Pedido idPedido;
+	private int unidades;
+	private double precio;
+	
+	public PedidoProducto() {
+		
+	}
+
+	public PedidoProducto(int idPedidoProducto, Producto idProducto, Pedido idPedido, int unidades, double precio) {
+		super();
+		this.idPedidoProducto = idPedidoProducto;
+		this.idProducto = idProducto;
+		this.idPedido = idPedido;
+		this.unidades = unidades;
+		this.precio = precio;
+	}
+
+	public int getIdPedidoProducto() {
+		return idPedidoProducto;
+	}
+
+	public void setIdPedidoProducto(int idPedidoProducto) {
+		this.idPedidoProducto = idPedidoProducto;
+	}
+
+	public Producto getIdProducto() {
+		return idProducto;
+	}
+
+	public void setIdProducto(Producto idProducto) {
+		this.idProducto = idProducto;
+	}
+
+	public Pedido getIdPedido() {
+		return idPedido;
+	}
+
+	public void setIdPedido(Pedido idPedido) {
+		this.idPedido = idPedido;
+	}
+
+	public int getUnidades() {
+		return unidades;
+	}
+
+	public void setUnidades(int unidades) {
+		this.unidades = unidades;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	@Override
+	public String toString() {
+		return "PedidoProducto [idPedidoProducto=" + idPedidoProducto + ", idProducto=" + idProducto + ", unidades="
+				+ unidades + ", precio=" + precio + "]";
+	}
+	
+	
+
+}
