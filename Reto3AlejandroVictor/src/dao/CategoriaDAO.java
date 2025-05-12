a07b1798961f3cc6b5de3aac1cab3c234b12ed59
Vixx-gs <victorgallegoserrate@gmail.com>
2025-05-12 12:57:00 +0200

    dao a minuscula

---
+++ Reto3AlejandroVictor/src/dao/CategoriaDAO.java
@@ -0,0 +1,41 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import Clases.Categoria;
+import Util.Conexion;
+
+public class CategoriaDAO {
+	public static List<Categoria> lista() {
+		List<Categoria> lista = new ArrayList<Categoria>();  
+		try {
+			// Abro conexion
+			Connection con = Conexion.abreConexion();
+			// Creo Select
+
+			PreparedStatement pst = con
+					.prepareStatement("Select idCategoria, nombre " + "from categoria order by nombre asc");
+			// Metemos la consulta en un resultado de consultas
+			ResultSet rs = pst.executeQuery();
+
+			while (rs.next()) {
+				lista.add(new Categoria(rs.getInt("idcategoria"), 
+						rs.getString("nombre")));
+				
+			}
+			rs.close();
+		} catch (Exception e) {
+				e.printStackTrace();
+		}
+		finally {
+			Conexion.cierraConexion();
+		}
+		return lista;
+
+	}
+
+}
