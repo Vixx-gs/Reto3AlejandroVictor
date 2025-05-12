665737feea63a623dce7ed16d405de620ec3d4c8
Vixx-gs <victorgallegoserrate@gmail.com>
2025-05-12 13:50:15 +0200

    Clase PedidoDAO

---
+++ Reto3AlejandroVictor/src/dao/PedidoDAO.java
@@ -0,0 +1,41 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import Clases.Cliente;
+import Clases.Pedido;
+import Util.Conexion;
+
+public class PedidoDAO {
+	public static List<Pedido> lista() {
+		List<Pedido> lista = new ArrayList<Pedido>();  
+		try {
+			// Abro conexion
+			Connection con = Conexion.abreConexion();
+			// Creo Select
+
+			PreparedStatement pst = con
+					.prepareStatement("Select idCategoria, nombre " + "from categoria order by nombre asc");
+			// Metemos la consulta en un resultado de consultas
+			ResultSet rs = pst.executeQuery();
+			Cliente cl = new Cliente(rs.getInt("idcliente"), 
+					rs.getString("nombre"), rs.getString("direccion"), rs.getInt("codigo"));
+			while (rs.next()) {
+				lista.add(new Pedido(rs.getInt("idpedido"), cl, rs.getDouble("precioTotal"), rs.getString("direccionEnvio"), rs.getDate("fecha")));
+				
+			}
+			rs.close();
+		} catch (Exception e) {
+				e.printStackTrace();
+		}
+		finally {
+			Conexion.cierraConexion();
+		}
+		return lista;
+
+	}
+}
