ebed80adda6e7d74afa165716051a05afb173c1c
Vixx-gs <victorgallegoserrate@gmail.com>
2025-05-13 08:20:44 +0200

    Categoria inserta

--- Reto3AlejandroVictor/src/dao/CategoriaDAO.java
+++ Reto3AlejandroVictor/src/dao/CategoriaDAO.java
@@ -3,11 +3,14 @@ package dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
 import Clases.Categoria;
 import Util.Conexion;
+import clase.Curso;
+import util.conexion;
 
 public class CategoriaDAO {
 
@@ -37,6 +40,28 @@ public class CategoriaDAO {
 		}
 		return lista;
 
+	}
+	
+	public static void inserta(Categoria categoria)
+	{
+		try {
+			//abro conexion
+			Connection con = Conexion.abreConexion();
+			//creo select
+			PreparedStatement pst = con.prepareStatement("insert into Categoria(categoria) values (?,?)",Statement.RETURN_GENERATED_KEYS);
+			pst.setString(1, categoria.getNombre());
+			pst.execute();
+			//recupero clave
+			ResultSet rs = pst.getGeneratedKeys();
+			if(rs.next())
+				categoria.setIdCategoria(rs.getInt(1)); 
+			rs.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			Conexion.cierraConexion();
+		}
 	}
 
 }
